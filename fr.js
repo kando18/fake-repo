8cc5720c10ab4d3cf3b436dd005978c5e804e81e
Brian <becresearch@gmail.com>
2022-09-13 20:17:11 -0500

    add hello message

--- fr.js
+++ fr.js
@@ -1,6 +1,1 @@
 console.log('hello from fr.js');
-console.log('second line');
-console.log('third line');
-console.log('fourth line');
-console.log('fifth line');
-console.log('local message line');
