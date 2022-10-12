ddf02d38878d2cb64090e134890234a5b0b54b81
kando18 <45721343+kando18@users.noreply.github.com>
2022-10-12 14:38:31 -0500

    Delete pipeline.js

--- pipeline.js
+++
@@ -1,7 +0,0 @@
-console.log('pipeline.js');
-console.log('test run 2');
-console.log('test run 3');
-console.log('test run 4');
-console.log('test run 5');
-console.log('test run 6');
-console.log('test run 7');
